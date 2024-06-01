963bf337a494b4605f56372058b5c48741591c97
saifyahya <saifbarakat991@gmail.com>
2024-06-01 21:51:10 +0000

    front-end enhancements

--- front-end/restaurant/src/app/component/dropdown-menue/dropdown-menue.component.ts
+++ front-end/restaurant/src/app/component/dropdown-menue/dropdown-menue.component.ts
@@ -3,7 +3,7 @@ import { Category } from '../../model/category';
 import { CategoryServiceService } from '../../service/category-service.service';
 import { Observable, map } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { RouterLink,RouterLinkActive } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink,RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-dropdown-menue',
@@ -14,15 +14,21 @@ import { RouterLink,RouterLinkActive } from '@angular/router';
 })
 export class DropdownMenueComponent implements OnInit{
 categories: Category[]=[];
-
-constructor(private service:CategoryServiceService){}
+constructor(private service:CategoryServiceService, private router:Router){}
   ngOnInit(): void {
       this.getAllCategories();
+      
+
   }
 
   getAllCategories(): void{
 this.service.getAllCategories().subscribe(
   data=>this.categories=data);
   }
+  categoryDropdown(event :Event){
+    let category_id = (<HTMLSelectElement>event.target).value;
+this.router.navigateByUrl(`/category/id/${category_id}`)
+  }
+
 
 }
